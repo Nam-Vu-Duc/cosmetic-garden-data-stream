936a56ec2db0b0e84f636eb4ee1931e08d2e6c9a
namvu <vuducnama6@example.com>
2025-07-18 20:46:06 +0700

    initial superset

--- dim.py
+++ dim.py
@@ -13,9 +13,9 @@ load_dotenv()
 
 # kafka
 topics = [
-    'create',
+    # 'create',
     'update',
-    'delete',
+    # 'delete',
 ]
 admin_client = AdminClient({'bootstrap.servers': 'localhost:9092'})
 consumer = Consumer({
@@ -43,7 +43,199 @@ products = db['products']
 brands = db['brands']
 orders = db['orders']
 
-def streaming_data():
+def de_active_current_record(topic_type: str, data: dict) -> None:
+    cur = conn.cursor()
+    if topic_type == 'customer':
+        cur.execute("""
+            UPDATE data_warehouse.dim_users 
+            SET end_date = CURRENT_TIMESTAMP, is_current = FALSE
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        conn.commit()
+        return
+
+    if topic_type == 'product':
+        cur.execute("""
+            UPDATE data_warehouse.dim_products
+            SET end_date = CURRENT_TIMESTAMP, is_current = FALSE
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        conn.commit()
+        return
+
+    if topic_type == 'brand':
+        cur.execute("""
+            UPDATE data_warehouse.dim_brands
+            SET end_date = CURRENT_TIMESTAMP, is_current = FALSE
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        conn.commit()
+        return
+
+    if topic_type == 'order':
+        cur.execute("""
+            UPDATE data_warehouse.dim_orders
+            SET end_date = CURRENT_TIMESTAMP, is_current = FALSE
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        conn.commit()
+        return
+
+    return None
+
+def insert_new_record(topic_type: str, data: dict) -> None:
+    cur = conn.cursor()
+    if topic_type == 'customer':
+        cur.execute("""
+            INSERT INTO data_warehouse.dim_users (
+                id, email, role, name, phone, gender, address, quantity, revenue, member_code
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+            str(data['_id']),
+            data['email'],
+            data['role'],
+            data['name'],
+            data['phone'],
+            'F' if data['gender'] == 'female' else 'M',
+            data['address'],
+            data['quantity'],
+            data['revenue'],
+            data['memberCode'],
+        ))
+
+        cur.execute("""
+            SELECT sk 
+            FROM data_warehouse.dim_users 
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        new_user_sk = cur.fetchone()[0]
+
+        fact_tables = [
+            "fact_page_view",
+            "fact_product_view",
+            "fact_brand_view",
+            "fact_cart_update",
+            "fact_purchase",
+            "fact_auth_update"
+        ]
+
+        # Update all fact tables
+        for table in fact_tables:
+            cur.execute(f"""
+                UPDATE data_warehouse.{table}
+                SET user_sk = %s
+                WHERE user_id = %s
+            """, (new_user_sk, str(data['_id'])))
+
+        conn.commit()
+        return
+
+    if topic_type == 'product':
+        cur.execute("""
+            INSERT INTO data_warehouse.dim_products (
+                id, categories, skincare, makeup, brand, name,
+                purchase_price, old_price, price, quantity,
+                status, rate, sale_number, rate_number
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+            str(data['_id']),
+            data['categories'],
+            data['skincare'],
+            data['makeup'],
+            data['brand'],
+            data['name'],
+            data['purchasePrice'],
+            data['oldPrice'],
+            data['price'],
+            data['quantity'],
+            data['status'],
+            data['rate'],
+            data['saleNumber'],
+            data['rateNumber']
+        ))
+
+        cur.execute("""
+            SELECT sk 
+            FROM data_warehouse.dim_products
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        new_product_sk = cur.fetchone()[0]
+
+        fact_tables = [
+            "fact_product_view",
+            "fact_cart_update",
+        ]
+
+        # Update all fact tables
+        for table in fact_tables:
+            cur.execute(f"""
+                UPDATE data_warehouse.{table}
+                SET product_sk = %s
+                WHERE product_id = %s
+            """, (new_product_sk, str(data['_id'])))
+
+        conn.commit()
+        return
+
+    if topic_type == 'brand':
+        cur.execute("""
+            INSERT INTO data_warehouse.dim_brands (
+                id, name, total_product, total_revenue
+            ) VALUES (%s, %s, %s, %s)
+            """, (
+            str(data['_id']),
+            data['name'],
+            data['totalProduct'],
+            data['totalRevenue'],
+        ))
+
+        cur.execute("""
+            SELECT sk 
+            FROM data_warehouse.dim_brands
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        new_brand_sk = cur.fetchone()[0]
+
+        cur.execute(f"""
+            UPDATE data_warehouse.fact_brand_view
+            SET brand_sk = %s
+            WHERE brand_id = %s
+        """, (new_brand_sk, str(data['_id'])))
+
+        conn.commit()
+        return
+
+    if topic_type == 'order':
+        cur.execute("""
+            INSERT INTO data_warehouse.dim_orders (
+                id, user_id, total_order_price, payment_method, status
+                ) VALUES (%s, %s, %s, %s, %s)
+            """, (
+            str(data['_id']),
+            data['customerInfo']['userId'],
+            data['totalNewOrderPrice'],
+            data['paymentMethod'],
+            data['status'],
+        ))
+
+        cur.execute("""
+            SELECT sk 
+            FROM data_warehouse.dim_orders
+            WHERE id = %s AND is_current = TRUE
+        """, (str(data['_id']),))
+        new_order_sk = cur.fetchone()[0]
+
+        cur.execute(f"""
+            UPDATE data_warehouse.fact_purchase
+            SET order_sk = %s
+            WHERE order_id = %s
+        """, (new_order_sk, str(data['_id'])))
+        conn.commit()
+        return
+
+    return None
+
+def streaming_data() -> None:
     new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
 
     # Create topics
@@ -57,7 +249,7 @@ def streaming_data():
         except Exception as e:
             print(f"{topic} already start !!!")
 
-def storing_data_warehouse():
+def storing_data_warehouse() -> None:
     try:
         while True:
             msg = consumer.poll(timeout=1.0)
@@ -73,85 +265,18 @@ def storing_data_warehouse():
 
             # Get topic, user_id, timestamp
             topic = msg.topic()
+            topic_type = data['topic_type']
+            body = data['body']
 
             if topic == 'create':
-                cur = conn.cursor()
-                if type == 'users':
-                    cur.execute("""
-                        INSERT INTO data_warehouse.dim_users (
-                            id, email, role, name, phone, gender, address, quantity, revenue, member_code
-                        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                        """, (
-                        str(data['_id']),
-                        data['email'],
-                        data['role'],
-                        data['name'],
-                        data['phone'],
-                        'F' if data['gender'] == 'female' else 'M',
-                        data['address'],
-                        data['quantity'],
-                        data['revenue'],
-                        data['memberCode'],
-                    ))
-                    conn.commit()
-
-                if type == 'product':
-                    cur.execute("""
-                        INSERT INTO data_warehouse.dim_products (
-                            id, categories, skincare, makeup, brand, name,
-                            purchase_price, old_price, price, quantity,
-                            status, rate, sale_number, rate_number
-                        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                        """, (
-                        str(data['_id']),
-                        data['categories'],
-                        data['skincare'],
-                        data['makeup'],
-                        data['brand'],
-                        data['name'],
-                        data['purchasePrice'],
-                        data['oldPrice'],
-                        data['price'],
-                        data['quantity'],
-                        data['status'],
-                        data['rate'],
-                        data['saleNumber'],
-                        data['rateNumber']
-                    ))
-                    conn.commit()
-
-                if type == 'brand':
-                    cur.execute("""
-                        INSERT INTO data_warehouse.dim_brands (
-                            id, name, total_product, total_revenue
-                        ) VALUES (%s, %s, %s, %s)
-                        """, (
-                        str(data['_id']),
-                        data['name'],
-                        data['totalProduct'],
-                        data['totalRevenue'],
-                    ))
-                    conn.commit()
-
-                if type == 'order':
-                    cur.execute("""
-                        INSERT INTO data_warehouse.dim_orders (
-                            id, user_id, total_order_price, payment_method, status
-                            ) VALUES (%s, %s, %s, %s, %s)
-                        """, (
-                        str(data['_id']),
-                        data['customerInfo']['userId'],
-                        data['totalNewOrderPrice'],
-                        data['paymentMethod'],
-                        data['status'],
-                    ))
-                    conn.commit()
+                insert_new_record(topic_type, body)
 
             if topic == 'update':
-                continue
+                de_active_current_record(topic_type, body)
+                insert_new_record(topic_type, body)
 
             if topic == 'delete':
-                continue
+                de_active_current_record(topic_type, body)
 
     except Exception as e:
         print(e)
--- fact.py
+++ fact.py
@@ -10,6 +10,8 @@ from confluent_kafka import Consumer, KafkaException
 from confluent_kafka.admin import AdminClient, NewTopic
 from pyflink.table import EnvironmentSettings, TableEnvironment
 from json import loads
+from datetime import datetime
+
 load_dotenv()
 
 # kafka
@@ -24,7 +26,7 @@ topics = [
 admin_client = AdminClient({'bootstrap.servers': 'localhost:9092'})
 consumer = Consumer({
     'bootstrap.servers': 'localhost:9092',
-    'group.id': 'my-consumer-group8',
+    'group.id': 'my-consumer-group15',
     'auto.offset.reset': 'earliest'
 })
 consumer.subscribe(topics)
@@ -51,7 +53,7 @@ conn = psycopg2.connect(
     port="5432"
 )
 
-def streaming_data():
+def streaming_data() -> None:
     new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
 
     # Create topics
@@ -65,7 +67,7 @@ def streaming_data():
         except Exception as e:
             print(f"{topic} already start !!!")
 
-def detecting_fraud():
+def detecting_fraud() -> None:
     try:
         # Define a Kafka source table
         t_env.execute_sql("""
@@ -104,7 +106,7 @@ def detecting_fraud():
     except Exception as e:
         print(f"Error: {e}")
 
-def storing_data_lake():
+def storing_data_lake() -> None:
     try:
         while True:
             msg = consumer.poll(timeout=1.0)
@@ -145,62 +147,99 @@ def storing_data_lake():
     finally:
         consumer.close()
 
-def storing_data_warehouse(object_key):
+def storing_data_warehouse(object_key) -> None:
     try:
         # get data from minIO
         response = s3.get_object(Bucket=bucket_name, Key=object_key)
         data = json.loads(response['Body'].read())
+        dt = datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        date_key = int(dt.strftime("%Y%m%d"))
+
+        cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO data_warehouse.dim_date (date_key, year, quarter, month, day, weekday)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT (date_key) DO NOTHING;
+        """, (
+            date_key,
+            dt.year,
+            (dt.month - 1) // 3 + 1,
+            dt.month, dt.day,
+            dt.strftime("%A")
+        ))
+        conn.commit()
 
         # get topic from object_key
         topic = object_key.split('/')[0]
 
         # insert into fact tables
-        cur = conn.cursor()
         if topic == 'page-view':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_page_view (user_sk, page_type, timestamp) VALUES (%s, %s, %s)
-                """, (
-                data['user_id'], data['page_type'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_page_view (user_id, user_sk, page_type, timestamp, date_key)
+                SELECT %s, sk, %s, %s, %s
+                FROM data_warehouse.dim_users
+                WHERE id = %s AND is_current = TRUE
+            """, (
+                data['user_id'], data['page_type'], data['timestamp'], data['user_id'], date_key
             ))
             conn.commit()
 
         if topic == 'product-view':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_product_view (user_sk, product_sk, timestamp) VALUES (%s, %s, %s)
-                """, (
-                data['user_id'], data['product_id'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_product_view (user_id, user_sk, product_id, product_sk, timestamp, date_key)
+                SELECT %s, u.sk, %s, p.sk, %s, %s
+                FROM data_warehouse.dim_users u, data_warehouse.dim_products p
+                WHERE u.id = %s AND u.is_current = TRUE
+                  AND p.id = %s AND p.is_current = TRUE
+            """, (
+                data['user_id'], data['product_id'], data['timestamp'], data['user_id'], data['product_id'], date_key
             ))
             conn.commit()
 
         if topic == 'brand-view':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_brand_view (user_sk, brand_sk, timestamp) VALUES (%s, %s, %s)
-                """, (
-                data['user_id'], data['brand_id'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_brand_view (user_id, user_sk, brand_id, brand_sk, timestamp, date_key)
+                SELECT %s, u.sk, %s, b.sk, %s, %s
+                FROM data_warehouse.dim_users u, data_warehouse.dim_brands b
+                WHERE u.id = %s AND u.is_current = TRUE
+                  AND b.id = %s AND b.is_current = TRUE
+            """, (
+                data['user_id'], data['brand_id'], data['timestamp'], data['user_id'], data['brand_id'], date_key
             ))
             conn.commit()
 
         if topic == 'purchase':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_purchase (user_sk, order_sk, timestamp) VALUES (%s, %s, %s)
-                """, (
-                data['user_id'], data['order_id'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_purchase (user_id, user_sk, order_id, order_sk, timestamp, date_key)
+                SELECT %s, u.sk, %s, o.sk, %s, %s
+                FROM data_warehouse.dim_users u, data_warehouse.dim_orders o
+                WHERE u.id = %s AND u.is_current = TRUE
+                  AND o.id = %s AND o.is_current = TRUE
+            """, (
+                data['user_id'], data['order_id'], data['timestamp'], data['user_id'], data['order_id'], date_key
             ))
             conn.commit()
 
         if topic == 'cart-update':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_cart_update (user_sk, product_sk, update_type, timestamp) VALUES (%s, %s, %s, %s)
-                """, (
-                data['user_id'], data['product_id'], data['update_type'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_cart_update (user_id, user_sk, product_id, product_sk, update_type, timestamp, date_key)
+                SELECT %s, u.sk, %s, p.sk, %s, %s, %s
+                FROM data_warehouse.dim_users u, data_warehouse.dim_products p
+                WHERE u.id = %s AND u.is_current = TRUE
+                  AND p.id = %s AND p.is_current = TRUE
+            """, (
+                data['user_id'], data['product_id'], data['update_type'], data['timestamp'], data['user_id'], data['product_id'], date_key
             ))
             conn.commit()
 
         if topic == 'auth-update':
             cur.execute("""
-                INSERT INTO data_warehouse.fact_auth_update (user_sk, update_type, timestamp) VALUES (%s, %s, %s)
-                """, (
-                data['user_id'], data['update_type'], data['timestamp'],
+                INSERT INTO data_warehouse.fact_auth_update (user_id, user_sk, update_type, timestamp, date_key)
+                SELECT %s, u.sk, %s, %s, %s
+                FROM data_warehouse.dim_users u
+                WHERE u.id = %s AND u.is_current = TRUE
+            """, (
+                data['user_id'], data['update_type'],  data['timestamp'], data['user_id'], date_key
             ))
             conn.commit()
 
